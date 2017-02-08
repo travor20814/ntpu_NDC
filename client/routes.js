14dbf84f1cfdde671f7e47eef27853948b689620
travor <travor20814@gmail.com>
2017-02-08 16:15:24 +0800

    add redux store

--- client/routes.js
+++ client/routes.js
@@ -1,5 +1,29 @@
+import 'babel-polyfill';
 import React from 'react';
-import { Router, Route, IndexRoute, browserHistory as history } from 'react-router';
+import thunk from 'redux-thunk';
+import {
+  createStore,
+  compose,
+  applyMiddleware,
+} from 'redux';
+import {
+  Provider,
+} from 'react-redux';
+import {
+  Router,
+  Route,
+  IndexRoute,
+  useRouterHistory,
+} from 'react-router';
+import fetchMiddleware from 'redux-middleware-fetch';
+import {
+  syncHistoryWithStore,
+  routerMiddleware,
+} from 'react-router-redux';
+import {
+  createHistory,
+} from 'history';
+import reducer from './reducers/index.js';
 
 // Boards
 import SiteLayout from './components/SiteLayout.js';
@@ -8,13 +32,29 @@ import PageJudges from './components/PageJudges.js';
 import PageAbout from './components/PageAbout.js';
 import PageCompetition from './components/PageCompetition.js';
 
+const history = useRouterHistory(createHistory)({
+  basename: '/',
+});
+
+export const store = createStore(reducer, {}, compose(
+  applyMiddleware(
+    thunk,
+    routerMiddleware(history),
+    fetchMiddleware,
+  ),
+));
+
+const storeHistory = syncHistoryWithStore(history, store);
+
 export default (
-  <Router history={history}>
-    <Route path="/" component={SiteLayout}>
-      <IndexRoute component={SiteIndex} />
-      <Route path="/judges" component={PageJudges} pathKey="judges" />
-      <Route path="/about" component={PageAbout} pathKey="about" />
-      <Route path="/competition" component={PageCompetition} pathKey="competition" />
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={storeHistory}>
+      <Route path="/" component={SiteLayout}>
+        <IndexRoute component={SiteIndex} />
+        <Route path="/judges" component={PageJudges} pathKey="judges" />
+        <Route path="/about" component={PageAbout} pathKey="about" />
+        <Route path="/competition" component={PageCompetition} pathKey="competition" />
+      </Route>
+    </Router>
+  </Provider>
 );
